7adb84b1b4dccd338acd23a50d27910652a26d7e
avosa <websteravosa@gmail.com>
2021-08-03 01:14:37 +0300

    Initial Commit

---
+++ Befriend macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Befriend macOS
+//
+//  Created by webster on 8/3/21.
+//  Copyright © 2021 befirend. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
